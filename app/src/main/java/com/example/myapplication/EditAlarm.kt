ce1d674a036a1a014f7b834ed45a5c4574f84405
Allexsan <sasha.ermatov05@mail.ru>
2023-03-13 11:01:31 +0500

    first commit

---
+++ app/src/main/java/com/example/myapplication/EditAlarm.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAlarm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_alarm)
+    }
+}
